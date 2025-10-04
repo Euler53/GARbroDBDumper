5041712cce4e2ab57f43e257450f3dcc575a02e3
Euler <69144253+Euler53@users.noreply.github.com>
2025-10-04 10:40:36 +0900

    Update Program.cs

--- SchemeDumper/Program.cs
+++ SchemeDumper/Program.cs
@@ -1,28 +1,74 @@
 using GameRes;
 using GameRes.Compression;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Reflection;
 using System.Runtime.Serialization.Formatters.Binary;
 
 namespace SchemeDumper
 {
+    public class ForceAllFieldsResolver : DefaultContractResolver
+    {
+        protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization)
+        {
+            var props = base.CreateProperties(type, memberSerialization);
+
+            var fields = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+            foreach (var field in fields)
+            {
+                if (!props.Any(p => p.PropertyName == field.Name))
+                {
+                    var prop = base.CreateProperty(field, memberSerialization);
+
+                    prop.Readable = true; 
+
+                    props.Add(prop);
+                }
+            }
+
+            return props;
+        }
+    }
+
     internal static class Program
     {
         private static void Main(string[] args)
         {
-            using (Stream stream = File.OpenRead(".\\GameData\\Formats.dat"))
+            try
             {
-                int version = FormatCatalog.Instance.GetSerializedSchemeVersion(stream);
-                using (ZLibStream zs = new ZLibStream(stream, CompressionMode.Decompress, true))
+                using (Stream stream = File.OpenRead(".\\GameData\\Formats.dat"))
                 {
-                    BinaryFormatter bin = new BinaryFormatter();
-                    SchemeDataBase db = (SchemeDataBase)bin.Deserialize(zs);
-                    string json = JsonConvert.SerializeObject(db, Formatting.Indented);
-                    File.WriteAllText(".\\GameData\\Formats.json", json);
+                    FormatCatalog.Instance.GetSerializedSchemeVersion(stream);
+
+                    using (ZLibStream zs = new ZLibStream(stream, CompressionMode.Decompress, true))
+                    {
+                        var bin = new BinaryFormatter();
+                        var db = (SchemeDataBase)bin.Deserialize(zs);
+
+                        var settings = new JsonSerializerSettings
+                        {
+                            TypeNameHandling = TypeNameHandling.Objects,
+                            ContractResolver = new ForceAllFieldsResolver(),
+                            Formatting = Formatting.Indented
+                        };
+
+                        string json = JsonConvert.SerializeObject(db, settings);
+                        File.WriteAllText(".\\GameData\\Formats.json", json);
+                    }
                 }
+                Console.WriteLine("Scheme dumped successfully (including all fields).");
             }
-            Console.WriteLine("Scheme dumped.");
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine("An error occurred: " + ex.Message);
+            }
+
+            Console.WriteLine("Press Enter to exit.");
             Console.ReadLine();
         }
     }
